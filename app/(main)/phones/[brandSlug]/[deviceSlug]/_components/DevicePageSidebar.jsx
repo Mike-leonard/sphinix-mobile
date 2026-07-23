bca734d8827b5c89f6362f57c2fffb106424a342
leonard <mortuza.7@gmail.com>
2026-07-23 15:59:08 +0200

    fix: convert DevicePageSidebar to Server Component to support async sidebar components

--- app/(main)/phones/[brandSlug]/[deviceSlug]/_components/DevicePageSidebar.jsx
+++ app/(main)/phones/[brandSlug]/[deviceSlug]/_components/DevicePageSidebar.jsx
@@ -1,22 +1,12 @@
-'use client';
-import React, { useState } from 'react';
+import React from 'react';
 import RightSidebar from '@/components/sidebar/RightSidebar';
 
 export default function DevicePageSidebar({ newArrivals, topRated }) {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [selectedBrand, setSelectedBrand] = useState("All");
-
   return (
     <RightSidebar
-      searchQuery={searchQuery}
-      setSearchQuery={setSearchQuery}
-      selectedBrand={selectedBrand}
-      setSelectedBrand={setSelectedBrand}
       selectedCategory="All"
-      setSelectedCategory={() => {}}
       newArrivals={newArrivals}
       topRated={topRated}
-      brands={[]}
       isDevicesRoute={true}
     />
   );
